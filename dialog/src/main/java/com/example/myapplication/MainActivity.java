cf26e7fe09122f6cc42ee3b01a703de0ac7a5207
sdcounter <sdcounter@gmail.com>
2015-11-05 11:30:35 +0900

    a

---
+++ dialog/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("사용자 정보 입력");
+
+        tvName = (EditText) findViewById(R.id.tvName);
+        tvEmail = (EditText) findViewById(R.id.tvEmail);
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog1, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+
+                dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+
+                dlgEdtName.setText(tvName.getText().toString());
+                dlgEdtEmail.setText(tvEmail.getText().toString());
+                dlg.setTitle("사용자 정보 입력");
+           //     dlg.setIcon(R.drawable.ic_menu_allfriends);
+                dlg.setView(dialogView);
+
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                        int xOffset = (int) (Math.random() * display.getWidth());
+                        int yOffset = (int) (Math.random() * display.getHeight());
+                        Toast toast = new Toast(MainActivity.this);
+                        toastView = (View) View.inflate(MainActivity.this, R.layout.toast1, null);
+                        toastText = (TextView) toastView.findViewById(R.id.toastText1);
+                        toastText.setText("취소했습니다");
+                        toast.setView(toastView);
+                        toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
+                        toast.show();
+                    }
+                });
+                dlg.show();
+            }
+        });
+    }
+}
